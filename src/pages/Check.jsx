d9a2f7e841c5ea598c627a3c88f408059d69da4e
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2021-01-22 14:21:05 +0530

    added dashboard ConnectReq, nav & reviewBox

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -3,13 +3,19 @@ import ShowInfoCard from '../components/Landing/ShowInfoCard'
 import Filter from '../components/myEvents/Filter'
 import Upper from '../components/myEvents/Upper'
 import Sidebar from '../components/Dashboard/Sidebar'
+import Nav from '../components/Dashboard/Nav'
+import ReviewBox from '../components/Dashboard/ReviewBox'
+import ConnectionReq from '../components/Dashboard/ConnectionReq'
 function Check() {
     return (
         <div>
             {/* <Upper />
             <Filter />   */}
-            <ShowInfoCard />   
-            <Sidebar />
+            {/* <ShowInfoCard />    */}
+            {/* <Sidebar /> */}
+            <Nav />
+            <ReviewBox />
+            <ConnectionReq />
         </div>
     )
 }
